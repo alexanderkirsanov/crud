cf0df1ce577e16e5d16a27417c45d4a931560b04
lqip32 <lqip32@gmail.com>
2011-06-19 16:42:45 +0600

    add ant build file for teamcity system

---
+++ crud/test/ru/susu/crud/MainTest.java
@@ -0,0 +1,14 @@
+package ru.susu.crud;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+	@Test
+	public void junitTest(){
+		assertEquals(2,1+1);
+	}
+	
+
+}
