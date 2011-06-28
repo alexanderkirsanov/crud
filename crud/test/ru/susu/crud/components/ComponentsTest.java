4de6e51e230984c794a569f064857bf664aac5ac
lqip32 <lqip32@gmail.com>
2011-06-29 00:56:59 +0600

    Correct commit

---
+++ crud/test/ru/susu/crud/components/ComponentsTest.java
@@ -0,0 +1,43 @@
+package ru.susu.crud.components;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ru.susu.crud.components.Component;
+import ru.susu.crud.components.TextBox;
+
+
+public class ComponentsTest {
+	Component testComponent;
+	
+	@Before
+	public void setUp(){
+		testComponent = new Component("1");
+	}
+	
+	@Test
+	public void testCreateComponent(){
+		assertEquals(testComponent.getName(),"1");
+	}
+	
+	@Test
+	public void testGetAllowNullValue(){
+		assertTrue(testComponent.getAllowNullValue());
+	}
+	
+	@Test
+	public void testSetAllowNullValue(){
+		testComponent.setAllowNullValue(false);
+		assertFalse(testComponent.getAllowNullValue());
+	}
+	
+	@Test
+	public void testCreateTextBox(){
+		Component testTextBox = new TextBox("1");
+		assertEquals(testTextBox.getName(), "1");
+	}
+
+	
+}
