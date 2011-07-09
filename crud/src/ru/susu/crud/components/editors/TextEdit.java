44a8b1d443d76840a98402e518df2b6a5ff0e585
manb9lkk <manb9lkk@gmail.com>
2011-07-10 00:40:37 +0600

    add the methods in EditorsTest

--- crud/src/ru/susu/crud/components/editors/TextEdit.java
+++ crud/src/ru/susu/crud/components/editors/TextEdit.java
@@ -9,12 +9,64 @@ package ru.susu.crud.components.editors;
  */
 public class TextEdit {
     private String name;
+    private int size;
+    private int maxLength;
+    private boolean passwordMode;
+    private Object value;
+    private boolean allowHtmlCharacters;
 
     public TextEdit(String name) {
         this.name = name;
+        this.allowHtmlCharacters = true;
+    }
+
+    public TextEdit(String name, int size, int maxLength) {
+        this(name);
+        this.size = size;
+        this.maxLength = maxLength;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getMaxLength() {
+        return maxLength;
+    }
+
+    public void setMaxLength(int maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    public boolean getPasswordMode() {
+        return passwordMode;
+    }
+
+    public void setPasswordMode(boolean passwordMode) {
+        this.passwordMode = passwordMode;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public boolean getAllowHtmlCharacters() {
+        return allowHtmlCharacters;
+    }
+
+    public void setAllowHtmlCharacters(boolean allowHtmlCharacters) {
+        this.allowHtmlCharacters = allowHtmlCharacters;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
     }
 }
--- crud/test/ru/susu/crud/components/editors/EditorTest.java
+++ crud/test/ru/susu/crud/components/editors/EditorTest.java
@@ -4,13 +4,7 @@ import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
-/**
- * Created by IntelliJ IDEA.
- * User: dem
- * Date: 09.07.11
- * Time: 15:36
- * To change this template use File | Settings | File Templates.
- */
+//TODO ExtractsValueFromPost for Editors!!!
 public class EditorTest {
 
     @Test
@@ -18,5 +12,32 @@ public class EditorTest {
         String name = "name";
         TextEdit testTE = new TextEdit(name);
         assertEquals(testTE.getName(), name);
+    }
+
+    @Test
+    public void textEditConfigureTest() {
+        TextEdit testTE = new TextEdit("name", 10, 10);
+        assertEquals(testTE.getName(), "name");
+        assertEquals(testTE.getSize(), 10);
+        assertEquals(testTE.getPasswordMode(), false);
+        assertEquals(testTE.getMaxLength(), 10);
+        testTE.setSize(25);
+        testTE.setMaxLength(30);
+        testTE.setPasswordMode(true);
+        assertEquals(testTE.getSize(), 25);
+        assertEquals(testTE.getMaxLength(),30);
+        assertEquals(testTE.getPasswordMode(), true);
+    }
+
+    @Test
+    public void textEditGetValueAndAllowHtmlCharactersTest() {
+        TextEdit testTE = new TextEdit("name", 1, 1);
+        Object testValue = new Object();
+        assertEquals(testTE.getValue(), null);
+        assertEquals(testTE.getAllowHtmlCharacters(), true);
+        testTE.setValue(testValue);
+        testTE.setAllowHtmlCharacters(false);
+        assertEquals(testTE.getValue(), testValue);
+        assertEquals(testTE.getAllowHtmlCharacters(), false);
     }
 }
