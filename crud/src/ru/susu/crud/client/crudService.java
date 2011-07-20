5d7ab988f4ab2293b3d2c9e3c4fc33a3e35dd84c
manb9lkk <manb9lkk@gmail.com>
2011-07-21 00:21:59 +0600

    commit before pulling changes from repository

--- crud/src/ru/susu/crud/client/crudService.java
+++ crud/src/ru/susu/crud/client/crudService.java
@@ -5,7 +5,6 @@ import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 import java.util.List;
-import java.util.Map;
 
 @RemoteServiceRelativePath("crudService")
 public interface crudService extends RemoteService {
@@ -14,9 +13,10 @@ public interface crudService extends RemoteService {
 
     List<String> getTables();
 
-    void setTable(String tableName);
 
-    Map<String, String> update();
+
+
+    void setTable(String tableName);
 
     List<String[]> getData(String s);
 
